7897ab92e550257c9106460b48456a8c7a479e0d
Gabriel <gpanassol@gmail.com>
2019-07-31 10:59:27 -0300

    Terminado aula 2

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -1,2 +1,7 @@
-let negociacao = new Negociacao(new Date(), 2, 100);
-console.log(negociacao);
+const controller = new NegociacaoController();
+
+// Adiciona no submit do form o controller
+document
+    .querySelector('.form')
+    .addEventListener('submit', controller.adiciona.bind(controller));
+    
