9e489be53bc654dc81fe3409f392b7684d9b115d
Gabriel <gpanassol@gmail.com>
2019-07-31 14:23:04 -0300

    Finalizado o curso parte 1

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -1,7 +1,3 @@
 const controller = new NegociacaoController();
 
-// Adiciona no submit do form o controller
-document
-    .querySelector('.form')
-    .addEventListener('submit', controller.adiciona.bind(controller));
-    
+$('.form').submit(controller.adiciona.bind(controller));
--- app/ts/views/NegociacoesView.ts
+++ app/ts/views/NegociacoesView.ts
@@ -1,16 +1,7 @@
-class NegociacoesView{
-    
-    private _elemento: Element;
+class NegociacoesView extends View<Negociacoes> {
 
-    constructor(seletor: string){
-        this._elemento = document.querySelector(seletor);
-    }
-    
-    update(model: Negociacoes): void{
-        this._elemento.innerHTML = this.template(model);
-    }
-    
-    template(model: Negociacoes) : string {
+    template(model: Negociacoes): string {
+
         return `
         <table class="table table-hover table-bordered">
             <thead>
@@ -23,20 +14,21 @@ class NegociacoesView{
             </thead>
 
             <tbody>
-            ${model.paraArray().map(negociacao => 
-                `
-                    <tr>
-                        <td>${negociacao.data.getDate()}/${negociacao.data.getMonth()+1}/${negociacao.data.getFullYear()}</td>
-                        <td>${negociacao.quantidade}</td>
-                        <td>${negociacao.valor}</td>
-                        <td>${negociacao.volume}</td>
-                    </tr>                        
-                `).join('')}  
+                ${model.paraArray().map(negociacao => 
+                     `
+                        <tr>
+                            <td>${negociacao.data.getDate()}/${negociacao.data.getMonth() +1}/${negociacao.data.getFullYear()}</td>
+                            <td>${negociacao.quantidade}</td>
+                            <td>${negociacao.valor}</td>
+                            <td>${negociacao.volume}</td>
+                        <tr>
+                     `   
+                ).join('')}            
             </tbody>
 
             <tfoot>
             </tfoot>
-        </table>  
+        </table> 
         `;
     }
 }
