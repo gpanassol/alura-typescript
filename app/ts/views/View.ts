ec388ecf25582ac1420757bd7b9f7bfcae7ab6a0
Gabriel <gpanassol@gmail.com>
2019-07-31 21:00:14 -0300

    Finalizado aula 2 parte 2

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,4 +1,4 @@
-abstract class View<T> {
+export abstract class View<T> {
 
     protected _elemento: JQuery;
 
@@ -15,4 +15,3 @@ abstract class View<T> {
     abstract template(model: T): string;
 
 }
-
